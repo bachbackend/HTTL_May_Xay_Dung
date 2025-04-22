ffbb7cc9b3e518d9829b9db462acfef48b8d4141
tungnthe160120 <tungnthe160120@fpt.edu.vn>
2025-04-22 11:39:18 +0700

    tungdzai

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HTTL_May_Xay_Dung.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+    }
+}
---
+++ DTO/RegisterDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HTTL_May_Xay_Dung.DTO
+{
+    public class RegisterDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public short Role {  get; set; }
+        public DateTime? CreateAt { get; set; }
+        public DateTime? LastLogin { get; set;}
+        public short status { get; set; }
+    }
+}
