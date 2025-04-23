f3b4195ce9d7123589df32a4144663f13accf7f9
BachLNHE160284 <bachlnhe160284@fpt.edu.vn>
2025-04-23 09:29:14 +0700

    bach update

--- wwwroot/images/Class.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace HTTL_May_Xay_Dung.wwwroot.images
-{
-    public class Class
-    {
-    }
-}
--- wwwroot/images/article/Class.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace HTTL_May_Xay_Dung.wwwroot.images.article
-{
-    public class Class
-    {
-    }
-}
