256d1a3af14de63cb5c986170365f9de2152e95d
tungnthe160120 <tungnthe160120@fpt.edu.vn>
2025-04-29 16:23:01 +0700

    updateeee

---
+++ DTO/ProfileDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace HTTL_May_Xay_Dung.DTO
+{
+    public class ProfileDTO
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phonenumber { get; set; } = string.Empty;
+        public DateTime? LastLogin { get; set; }
+        public short Role { get; set; }
+    }
+}
